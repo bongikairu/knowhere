659c744ce5ae338db5df6fea06343494c7c2012d
Sutas Nakasawek <sutas@arterex.com>
2018-01-31 21:22:45 +0700

    kinda looking good map

--- src/index.js
+++ src/index.js
@@ -6,105 +6,215 @@ import math from 'mathjs';
 
 let doc = null;
 
+const scale = 10;
+
+const path_width = 1.5;
+const path_neighbor_gap = 0.7;
+const path_pad_start = 3;
+const path_pad_end = 1;
+
+const stop_radius = 0.6;
+const stop_name_gap = 1;
+const stop_name_size = 2;
+const stop_font_style = "font-family: TH Sarabun New; font-weight: bold;";
+
 try {
     doc = yaml.safeLoad(fs.readFileSync('data.yml', 'utf8'));
-    console.log(doc);
+    // console.log(doc);
 } catch (e) {
     console.log(e);
+    process.exit(1);
 }
+
+// generate stops' lines from lines' stops
+_.map(doc.lines, (line) => {
+    const line_no = line.no;
+    _.map(line.stops, (stop_id) => {
+        const stop_index = _.findIndex(doc.stops, ({id}) => id === stop_id);
+        if (stop_index < 0) throw new Error(`stop ${stop_id} not found for line ${line_no}`);
+
+        doc.stops[stop_index].lines = [
+            ...(doc.stops[stop_index].lines || []),
+            (line.combo ? line.combo : [line_no]),
+        ];
+    });
+});
+
+_.map(doc.stops, (stop, stop_idx) => {
+    doc.stops[stop_idx].lines_real = doc.stops[stop_idx].lines || [];
+    doc.stops[stop_idx].lines = [
+        ...(Array(stop.pad_left || 0).fill([])),
+        ...(doc.stops[stop_idx].lines || []),
+        ...(Array(stop.pad_right || 0).fill([])),
+    ];
+    doc.stops[stop_idx].first_line_idx = stop.pad_left || 0;
+    doc.stops[stop_idx].last_line_idx = (stop.pad_left || 0) + doc.stops[stop_idx].lines_real.length - 1;
+});
+
+// console.log(doc.stops.map(({lines}) => lines));
 
 let output = "";
 let output2 = "";
 
 const add = (text) => output += text + "\n";
 const add2 = (text) => output2 += text + "\n";
-const yp2sp = (pos) => ({x: 500 + pos.x * 10, y: 1000 - 50 - pos.y * 10});
+const yp2sp = (pos) => ({x: 500 + pos.x * scale, y: 1000 - 200 - pos.y * scale});
 
-add(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="250 500 750 1000">`);
+add(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">`);
 
-let last_stop = _.mapValues(doc.lines, () => 0);
+add(`<rect x="0" y="0" width="1000" height="1000" fill="#DCDCDC" />`)
 
-_.map(doc.stops, (stop, current_stop_id) => {
-    _.map(stop.lines, (lines, group_i) => {
-        const line = lines[0];
-        const line_last_stop = last_stop[line];
-        console.log(`from ${line_last_stop} to ${current_stop_id}`);
+let strokes = {};
 
-        const start_dir = Victor.fromObject(doc.stops[line_last_stop].direction).norm().multiplyScalarY(-1);
-        const end_dir = Victor.fromObject(doc.stops[current_stop_id].direction).norm().multiplyScalarY(-1);
+const add_stroke = (line_no, text) => strokes[line_no] = [...(strokes[line_no] || []), text];
 
-        console.log(start_dir, end_dir);
+// _.map(doc.stops, (stop, current_stop_idx) => {
+//     _.map(stop.lines, (lines, group_i) => {
+
+_.map(doc.lines, (line, line_idx) => {
+    const line_no = line.no;
+    let last_stop_id = line.stops[0];
+    _.map(line.stops, (stop_id, stop_idx) => {
+
+        const last_stop = _.find(doc.stops, stop => stop.id === last_stop_id);
+        const current_stop = _.find(doc.stops, stop => stop.id === stop_id);
+
+        last_stop_id = stop_id;
+
+        console.log(`line ${line_no} from ${last_stop.id} to ${current_stop.id}`);
+
+        const start_dir = Victor.fromObject(last_stop.direction).norm().multiplyScalarY(-1);
+        const end_dir = Victor.fromObject(current_stop.direction).norm().multiplyScalarY(-1);
+
+        // console.log(start_dir, end_dir);
 
         const start_per_dir = start_dir.clone().rotate(Math.PI / 2);
         const end_per_dir = end_dir.clone().rotate(Math.PI / 2);
 
-        console.log(start_per_dir, end_per_dir);
+        // console.log(start_per_dir, end_per_dir);
 
-        const start_leftmost_count = -(doc.stops[line_last_stop].lines.length - 1) / 2;
-        const start_vec_leftmost = new Victor(start_leftmost_count, start_leftmost_count);
+        // 1 => 0p 0g
+        // 2 => 0.5p 0.5g
+        // 3 => 1p 1g
+        // 4 => 1.5p 1.5g
 
-        const end_leftmost_count = -(doc.stops[current_stop_id].lines.length - 1) / 2;
-        const end_vec_leftmost = new Victor(end_leftmost_count, end_leftmost_count);
+        const start_leftmost_count = -(last_stop.lines.length - 1) / 2;
+        const end_leftmost_count = -(current_stop.lines.length - 1) / 2;
 
         // console.log(leftmost, vec_leftmost);
 
-        const start_leftmost = start_per_dir.clone().multiply(start_vec_leftmost);
-        const end_leftmost = end_per_dir.clone().multiply(end_vec_leftmost);
+        const path_and_gap = path_width + path_neighbor_gap;
 
-        console.log(start_leftmost, end_leftmost);
+        const start_leftmost = start_per_dir.clone().multiplyScalar(start_leftmost_count * path_and_gap);
+        const end_leftmost = end_per_dir.clone().multiplyScalar(end_leftmost_count * path_and_gap);
 
-        const start_group_id = _.findIndex(doc.stops[line_last_stop].lines, (line_list) => line_list.indexOf(line) >= 0);
-        const end_group_id = group_i;
+        // console.log(start_leftmost, end_leftmost);
 
-        const start_vec_group = new Victor(start_group_id, start_group_id);
-        const end_vec_group = new Victor(end_group_id, end_group_id);
+        const start_group_id = _.findIndex(last_stop.lines, (line_list) => line_list.indexOf(line_no) >= 0);
+        const end_group_id = _.findIndex(current_stop.lines, (line_list) => line_list.indexOf(line_no) >= 0);
 
-        const start_current = start_leftmost.clone().add(start_per_dir.clone().multiply(start_vec_group));
-        const end_current = end_leftmost.clone().add(end_per_dir.clone().multiply(end_vec_group));
+        const start_current = start_leftmost.clone().add(start_per_dir.clone().multiplyScalar(start_group_id * path_and_gap)).multiplyScalar(scale);
+        const end_current = end_leftmost.clone().add(end_per_dir.clone().multiplyScalar(end_group_id * path_and_gap)).multiplyScalar(scale);
 
-        console.log(start_current, end_current);
+        // console.log(start_current, end_current);
 
-        const start_x = yp2sp(doc.stops[line_last_stop].position).x + 5 + start_current.x * 10;
-        const start_y = yp2sp(doc.stops[line_last_stop].position).y + 5 + start_current.y * 10;
-        const end_x = yp2sp(doc.stops[current_stop_id].position).x + 5 + end_current.x * 10;
-        const end_y = yp2sp(doc.stops[current_stop_id].position).y + 5 + end_current.y * 10;
+        const start_x_real = yp2sp(last_stop.position).x + start_current.x;
+        const start_y_real = yp2sp(last_stop.position).y + start_current.y;
+        const end_x_real = yp2sp(current_stop.position).x + end_current.x;
+        const end_y_real = yp2sp(current_stop.position).y + end_current.y;
 
-        const color = doc.lines[line].color;
+        if (current_stop.type === 'stop') {
+            add2(`<circle cx="${end_x_real}" cy="${end_y_real}" r="${stop_radius * scale}" fill= "#fff" stroke="#333333" stroke-width="0" />`);
 
-        if (current_stop_id !== 0) {
+            const is_upward = (new Victor(0, 1)).dot(end_dir) < 0;
+
+            let gap = end_per_dir.clone().multiplyScalar(stop_name_gap * scale).add(end_dir.clone().multiplyScalar(-stop_name_size * scale / 4));
+            let should_have_title = false;
+
+            if (end_group_id === current_stop.last_line_idx && is_upward) {
+                should_have_title = true;
+            } else if (end_group_id === current_stop.first_line_idx && !is_upward) {
+                gap = gap.clone().multiplyScalar(-1);
+                should_have_title = true;
+            }
+
+            if (should_have_title) {
+                add2(`<text x="${end_x_real + gap.x}" y="${end_y_real + gap.y}" style="${stop_font_style}" font-size="${stop_name_size * scale}">${current_stop.name}</text>`);
+            }
+        }
+
+        const start_pad = stop_idx === 1 ? start_dir.clone().multiplyScalar(-path_pad_start * scale) : new Victor(0, 0);
+        const end_pad = stop_idx === line.stops.length - 1 ? end_dir.clone().multiplyScalar(path_pad_end * scale) : new Victor(0, 0);
+
+        const start_x = start_x_real + start_pad.x;
+        const start_y = start_y_real + start_pad.y;
+        const end_x = end_x_real + end_pad.x;
+        const end_y = end_y_real + end_pad.y;
+
+        if (last_stop.id !== current_stop.id) {
 
             if (start_dir.clone().subtract(end_dir).length() < 0.000001) {
-                add(`<line x1="${start_x}" y1="${start_y}" x2="${end_x}" y2="${end_y}" stroke="${color}" stroke-width="6"/>`);
+
+                if ((new Victor(end_x_real - start_x_real, end_y_real - start_y_real)).norm().subtract(start_dir).length() > 0.000001) {
+                    console.log("== WARN: stops has same direction not aligning in straight line")
+                    console.log(new Victor(end_x_real - start_x_real, end_y_real - start_y_real).norm(), start_dir);
+                }
+
+                // add(`<line x1="${start_x}" y1="${start_y}" x2="${end_x}" y2="${end_y}" stroke="${color}" stroke-width="${path_width}"/>`);
+                add_stroke(line_no, `L${start_x},${start_y}`);
+                add_stroke(line_no, `L${end_x},${end_y}`);
             } else {
                 const intersect = math.intersect(
                     [start_x, start_y],
-                    [start_x + start_dir.x * 1000, start_y + start_dir.y * 1000],
-                    [end_x + end_dir.x * -1000, end_y + end_dir.y * -1000],
+                    [start_x + start_dir.x * 10000, start_y + start_dir.y * 10000],
+                    [end_x + end_dir.x * -10000, end_y + end_dir.y * -10000],
                     [end_x, end_y],
                 );
 
                 const mid_x = intersect[0];
                 const mid_y = intersect[1];
 
-                const mid_start_x = mid_x - start_dir.x * 10;
-                const mid_start_y = mid_y - start_dir.y * 10;
-                const mid_end_x = mid_x + end_dir.x * 10;
-                const mid_end_y = mid_y + end_dir.y* 10;
+                const mid_start_x = mid_x - start_dir.x * scale;
+                const mid_start_y = mid_y - start_dir.y * scale;
+                const mid_end_x = mid_x + end_dir.x * scale;
+                const mid_end_y = mid_y + end_dir.y * scale;
 
-                add(`<line x1="${start_x}" y1="${start_y}" x2="${mid_start_x}" y2="${mid_start_y}" stroke="${color}" stroke-width="6"/>`);
-                add(`<path fill="none" stroke="${color}" stroke-width="6" d="M${mid_start_x},${mid_start_y} Q${mid_x},${mid_y} ${mid_end_x},${mid_end_y}" />`);
-                add(`<line x1="${mid_end_x}" y1="${mid_end_y}" x2="${end_x}" y2="${end_y}" stroke="${color}" stroke-width="6"/>`);
+                // add(`<line x1="${start_x}" y1="${start_y}" x2="${mid_start_x}" y2="${mid_start_y}" stroke="${color}" stroke-width="${path_width}"/>`);
+                // add(`<path fill="none" stroke="${color}" stroke-width="${path_width}" d="M${mid_start_x},${mid_start_y} Q${mid_x},${mid_y} ${mid_end_x},${mid_end_y}" />`);
+                // add(`<line x1="${mid_end_x}" y1="${mid_end_y}" x2="${end_x}" y2="${end_y}" stroke="${color}" stroke-width="${path_width}"/>`);
+                add_stroke(line_no, `L${start_x},${start_y}`);
+                add_stroke(line_no, `L${mid_start_x},${mid_start_y}`);
+                add_stroke(line_no, `Q${mid_x},${mid_y}`);
+                add_stroke(line_no, `${mid_end_x},${mid_end_y}`);
+                add_stroke(line_no, `L${end_x},${end_y}`);
             }
-            _.map(lines, (line) => {
-                last_stop[line] = current_stop_id;
-            });
+            // _.map(lines, (line) => {
+            //     last_stop[line] = current_stop_idx;
+            // });
         }
 
-        if (stop.type === 'stop') {
-            add2(`<circle cx="${end_x}" cy="${end_y}" r="3" fill= "#fff" stroke="#333333" stroke-width="3px" />`);
-        }
     });
     // add(`<rect x="${yp2sp(stop.position).x}" y="${yp2sp(stop.position).y}" width="10" height="10" fill= "#fff" stroke="#333333" stroke-width="3px" />`);
+});
+
+// console.log(strokes);
+
+_.map(strokes, (dots, line_no) => {
+    const color = _.find(doc.lines, line => `${line.no}` === `${line_no}`).color;
+
+    const real_color = {
+        1: '#EE7733',
+        2: '#F8D130',
+        3: '#F3A34D',
+        4: '#BE9976',
+        5: '#27AA9E',
+        6: '#94328A',
+        7: '#F38330',
+        8: '#EB3233',
+    }[color] || color;
+
+    const path = dots.join(" ");
+    add(`<path fill="none" stroke="${real_color}" stroke-width="${path_width * scale}" d="M${dots[0].substring(1)} ${path}" />`);
 });
 
 // add(`<rect x="10" y="10" width="200" height="100" fill= "#fff" stroke="#333333" stroke-width="3px" />`);
